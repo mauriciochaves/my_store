1b6ffbcede9f260cf2d6b1885ec97fedd2ad8683
Mauricio Chaves <mcsj@cin.ufpe.br>
2019-03-12 19:28:46 -0300

    ajuste para o selenium server

--- config.js
+++ config.js
@@ -1,6 +1,7 @@
 exports.config = {
 
-    directConnect:true,
+    // directConnect:true,
+    seleniumAddress: 'http://selenium_server:4444/wd/hub',
     framework: 'jasmine2',
     specs:['test-case/*-test.js'],
     baseUrl:'http://automationpractice.com/index.php',
@@ -9,29 +10,40 @@ exports.config = {
         browser.manage().window().maximize();
         browser.ignoreSynchronization = true;
 
-        var JasmineHtmlReporter = require('protractor-jasmine2-html-reporter');
-        jasmine.getEnv().addReporter(new JasmineHtmlReporter({
-            savePath: 'reports',
-            screenshotsFolder:'./shots',
-            takeScreenshots:true,
-            cleanDestination:false,
-            fixedScreenshotName:true
-        }));
+        // var JasmineHtmlReporter = require('protractor-jasmine2-html-reporter');
+        // jasmine.getEnv().addReporter(new JasmineHtmlReporter({
+        //     savePath: 'reports',
+        //     screenshotsFolder:'./shots',
+        //     takeScreenshots:true,
+        //     cleanDestination:false,
+        //     fixedScreenshotName:true
+        // }));
+
+        var AllureReporter = require('jasmine-allure-reporter');
+        jasmine.getEnv().addReporter(new AllureReporter());
+        jasmine.getEnv().afterEach(function (done){
+            browser.takeScreenshot().then(function(png){
+                allure.createAttachment('Screenshot', function(){
+                    return new Buffer(png, 'base64')
+                }, 'image/png')();
+                done();
+            });
+        });
 
         var SpecReporter = require('jasmine-spec-reporter').SpecReporter;
         jasmine .getEnv().addReporter(new SpecReporter({
             spec:{
-       //      displayStacktrace: true,
-       //     displayErrorMessages: true,
-            displayFailed: true
-       //      displayDuration: true
+            displayStacktrace: true,
+           displayErrorMessages: true,
+            displayFailed: true,
+            displayDuration: true
                 },
             summary:{
              displayErrorMessages:true,
-       //      displayStacktrace: true,
+            displayStacktrace: true,
              displaySuccessful:true,
-             displayFailed:true
-       //      displayDuration:true
+             displayFailed:true,
+            displayDuration:true
             },
             colors:{
              enabled:true
